38a2f67a11b1bc6b03828e2a2c92ab3e03502d56
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2021-11-21 02:51:30 +0500

    fixed indents, x-scroll, hover states, swiper

--- app/js/scripts/swiper-config.js
+++ app/js/scripts/swiper-config.js
@@ -2,7 +2,7 @@ export function initSliders() {
 	const headerSwiper = new Swiper('.hero-swiper', {
 		loop: true,
 		initialSlide: 1,
-
+		effect: 'fade',
 		pagination: {
 			el: '.swiper-pagination',
 			dynamicBullets: true,
