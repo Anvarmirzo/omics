48d164dbace6716b0aa854f29edf2ce66b4d2483
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2021-11-22 23:15:47 +0500

    added transtion

--- app/js/scripts/swiper-config.js
+++ app/js/scripts/swiper-config.js
@@ -2,6 +2,12 @@ export function initSliders() {
 	const headerSwiper = new Swiper('.hero-swiper', {
 		loop: true,
 		initialSlide: 1,
+		speed: 1500,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+
 		effect: 'fade',
 		pagination: {
 			el: '.swiper-pagination',
